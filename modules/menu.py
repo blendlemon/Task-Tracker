30e001cc57c434ae92ee60e5a567c2ee713f75c2
Diego <diegogonzalezsoto1@gmail.com>
2025-04-30 18:44:02 +0200

    V 1.0

--- modules/menu.py
+++ modules/menu.py
@@ -1,7 +1,40 @@
 from modules.task import *
+import json
 
-tasklist = []
+TASK_FILE = "task.json"
+
+def load_task():
+    try:
+        with open (TASK_FILE, "r") as file:
+            task_data = json.load(file)
+            tasklist = []
+            for task in task_data:
+                tasklist.append(Task.to_obj(task))
+        return tasklist
+    except FileNotFoundError:
+        return []
+
+def save_task(tasklist):
+    with open (TASK_FILE, "w") as file:
+        task_data = []
+        for task in tasklist:
+            task_data.append(task.to_dict())
+        json.dump(task_data, file, indent=4)
+
+tasklist = load_task()
+
 while True:
+    print("""
+    Available commands:
+    - add <task name>: Adds a new task.
+    - mark-in-progress <id>: Marks a task as "in-progress".
+    - mark-done <id>: Marks a task as "done".
+    - update <id> <new description>: Updates the description of a task.
+    - delete <id>: Deletes a task.
+    - list: Displays all tasks.
+    - list <status>: Displays tasks filtered by status (e.g., "done", "in-progress").
+    - exit: Saves tasks and exits the program.
+    """)
     option = input("").lower()
     option = option.split(" ")
 
@@ -12,14 +45,33 @@ while True:
         case "mark-in-progress" | "mark-done":
             id = int(option[1])
             status = option[0][5::]
-            print(status)
-            Task.SearchTask(tasklist,id).UpdateStatus(status)
+            task = Task.SearchTask(tasklist,id)
+            if task != None:
+                task.UpdateStatus(status)
+
+        case "update":
+            id = int (option[1])
+            new = " ".join(option[2::])
+            task = Task.SearchTask(tasklist, id)
+            if task != None:
+                task.Update(new)
+        
+        case "delete":
+            id = int (option[1])
+            new = " ".join(option[2::])
+            task = Task.SearchTask(tasklist,id)
+            if task != None:
+                tasklist.pop(tasklist.index(task))
 
         case "list":
             if len(option)==1:
                 Task.ListAll(tasklist)
             else:
                 Task.listByStatus(tasklist,option[1])
+        case "exit":
+            save_task(tasklist)
+            exit()
+    
 
                     
 
--- modules/task.py
+++ modules/task.py
@@ -3,6 +3,9 @@ import time
 class Task:
 
     id = 0
+    @classmethod
+    def setId(cls, tasklist):
+        cls.id = tasklist[len(tasklist)-1].id
     
     def __init__(self, description):
 
@@ -12,6 +15,23 @@ class Task:
         self.status = "todo"
         self.createdAt = time.strftime("%Y-%m-%d %H:%M:%S")
         self.updatedAt = time.strftime("%Y-%m-%d %H:%M:%S")
+    
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "description": self.description,
+            "status": self.status,
+            "createdAt": self.createdAt,
+            "updatedAt": self.updatedAt
+        }
+    @staticmethod
+    def to_obj(dic):
+        task = Task(dic["description"])
+        task.id = dic["id"]
+        task.status = dic["status"]
+        task.createdAt = dic["createdAt"]
+        task.updatedAt = dic["updatedAt"]
+        return task
     
     def Update(self, description):
         self.description = description
@@ -40,4 +60,3 @@ class Task:
         for task in tasklist:
             if task.status == status:
                 print(vars(task))
-                
